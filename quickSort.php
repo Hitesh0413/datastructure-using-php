064fb235b668f16e181ef3097016bf37aa62a2b3
Hitesh Aloney <hiteshaloney75@gmail.com>
2019-06-09 23:03:59 +0530

    started implementing quick sort

---
+++ quickSort.php
@@ -0,0 +1,11 @@
+<?php
+include_once('generalFunctions.php');
+include_once('algoFunctions.php');
+
+$list=array(5,9,3,4,1,6,7,10,0,2);
+
+beautyOutput($list,'UNSORTED LIST');
+
+$result=quickSort($list);
+
+beautyOutput($result,'SORTED LIST');
